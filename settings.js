aef5d28133435cee4dd3a0235341cf3a05fdcf12
NyxCoreTech <nyxcoretech@gmail.com>
2026-05-08 14:04:55 +0100

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: '𝙽𝚈𝚇𝙲𝙾𝚁𝙴 𝙼𝙳',
+  author: '',
+  botName: "𝙽𝚈𝚇𝙲𝙾𝚁𝙴 𝙼𝙳",
+  botOwner: 'NyxCoreTech', // Your name
+  ownerNumber: '2349126367875', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "3.0.9",
+  updateZipUrl: "'",
+};
+
+module.exports = settings;
