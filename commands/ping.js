0f3e6c3103458dcca24e93ae65a0d3f626485e08
NyxCoreTech <nyxcoretech@gmail.com>
2026-05-08 12:05:47 +0100

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -29,7 +29,7 @@ async function pingCommand(sock, chatId, message) {
         const uptimeFormatted = formatTime(uptimeInSeconds);
 
         const botInfo = `
-┏━━〔 🤖 𝐋𝐀𝐒𝐄𝐑 𝐌𝐃 〕━━┓
+┏━━〔 🤖 NyxCore 𝐌𝐃 〕━━┓
 ┃ 🤩 Ping     : ${ping} ms
 ┃ ⏱️ Uptime   : ${uptimeFormatted}
 ┃ 🔖 Version  : v${settings.version}
