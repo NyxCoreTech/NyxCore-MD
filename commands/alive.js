bd6e6ad8ea383c2c53fac3213389e7801995201e
NyxCoreTech <nyxcoretech@gmail.com>
2026-05-08 11:44:06 +0100

    Update alive.js

--- commands/alive.js
+++ commands/alive.js
@@ -1,7 +1,7 @@
 const settings = require("../settings");
 async function aliveCommand(sock, chatId, message) {
     try {
-        const message1 = `*🤖 LASER MD is Active!*\n\n` +
+        const message1 = `*🤖 NyxCore MD is Active!*\n\n` +
                        `*Version:* ${settings.version}\n` +
                        `*Status:* Online\n` +
                        `*Mode:* Public\n\n` +
@@ -18,8 +18,8 @@ async function aliveCommand(sock, chatId, message) {
                 forwardingScore: 999,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363407561123100@newsletter',
-                    newsletterName: 'LASER MD',
+                    newsletterJid: '120363426242342797@newsletter',
+                    newsletterName: 'NyxCore MD',
                     serverMessageId: -1
                 }
             }
